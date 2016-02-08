862103cb1cbf89ab6f35b275dfd51d3b35682ada
Jamie King <king0120@gmail.com>
2016-02-08 11:18:57 -0500

    implemented navbar

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-  $('.button-collapse').sideNav();
-  console.log($('.button-collapse').sideNav({
-    edge: 'right',
-    closeOnClick: true
-  }));
-  console.log('hello world');
+  $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
+  console.log('right side');
   console.log('HI!');
 });
