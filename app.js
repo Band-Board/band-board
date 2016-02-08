cf8c5a3245ba99ac28463917286e5300fc3df15e
John Kirkley <john@johnkirkley.com>
2016-02-08 11:21:28 -0500

    passport configs

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+var passport = require('passport');
+var session = require('express-session');
+var flash = require('connect-flash');
+
 var Band = require('./models/band');
 
 //Routers
@@ -29,6 +33,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(session({ secret: 'WDI Rocks!' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./config/passport/passport')(passport);
+
+// This middleware will allow us to use the currentUser in our views and routes.
+app.use(function(req, res, next) {
+  global.currentUser = req.user;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
