59ff3b63c435b36e46809a3c6dfb27bb8a9a9b10
Spencer Lewis <Spencerlewis987@gmail.com>
2016-02-08 15:07:38 -0500

    DRYed up form to be included on new and edit views for bands.

--- controller/bands.js
+++ controller/bands.js
@@ -1,2 +1,10 @@
-var Band = require('../models/band');
+// var Band = require('../models/band');
 
+// //INDEX
+// function index() {
+// console.log('bands go here!');
+//   Band.find({})
+//   .then(function(bands) {
+//     res.render('bands/index', { bands: bands });
+//   });
+// };
