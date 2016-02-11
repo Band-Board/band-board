8a50eab955fe5a31f53bfecfc79c1f75b52dca07
Jamie King <king0120@gmail.com>
2016-02-11 13:19:54 -0500

    styling users and bands

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,18 @@
-
 $(document).ready(function() {
-  $('.button-collapse').sideNav({
-    menuWidth: 300, // Default is 240
-    edge: 'right', // Choose the horizontal origin
-    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-  });
+    $('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 240
+        edge: 'right', // Choose the horizontal origin
+        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+
+    });
+    if ($('.userProfile').length || $('.bandProfile').length) {
+        var JSON = [{
+            "title": "Demo event",
+            "start": "2016-2-15 22:20:00",
+            "allDay": true
+        }];
+        $('#calendar').fullCalendar({
+            events: JSON
+        });
+    }
 });
